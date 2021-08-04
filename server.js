6719564090605d207e376f329ff3467ff3399b0b
Özgür Coşkuner <ozgurcoskuner95@gmail.com>
2021-08-04 17:34:27 +0300

    variable name fixed

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('public'))
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log('server is running'))
